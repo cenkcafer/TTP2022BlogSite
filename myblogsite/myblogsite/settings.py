3123d51db7199370da45376f4dd19c75c2e1a89c
Michael Janus <46409972+bluemike61@users.noreply.github.com>
2022-01-12 23:40:36 -0500

    Updated backend with genghis code

--- myblogsite/myblogsite/settings.py
+++ myblogsite/myblogsite/settings.py
@@ -106,17 +106,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
-<<<<<<< Updated upstream
-=======
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
-
-
->>>>>>> Stashed changes
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
