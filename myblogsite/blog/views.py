b108254a2bc67b11db63cdee3bfaa6ec6f8b1706
Michael Janus <46409972+bluemike61@users.noreply.github.com>
2022-01-12 22:54:41 -0500

    Updated backend with genghis code

--- myblogsite/blog/views.py
+++ myblogsite/blog/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,  get_object_or_404
 from django.http import HttpResponse
+from .models import Blog
 # Create your views here.
 
 
 
 
-def index(request):
-    
-    return render(request, 'blog/index.html')
+def base(request):
+    return render(request, 'blog/base.html')
+
+
+
+def all_blogs(request):
+    blog_count = Blog.objects.count
+    blogs = Blog.objects.order_by('-date')
+    return render(request, 'blog/all_blogs.html', {'blogs': blogs})
+
+def detail(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog': blog})
 
