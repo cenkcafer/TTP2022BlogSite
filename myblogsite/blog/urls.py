6546d3e73a1779f6a2e713977be11f99270bbed3
robertquartey7 <37972474+robertquartey7@users.noreply.github.com>
2022-01-13 10:49:12 -0500

    .

--- myblogsite/blog/urls.py
+++ myblogsite/blog/urls.py
@@ -5,5 +5,6 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.all_blogs, name='all_blogs'),
+    path('contact/', views.contact, name='contact'),
     path('<int:blog_id>/', views.detail, name='detail'),
 ]
